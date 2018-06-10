ee2d1dd4c5c3e82da4664e50dbd03c5910056c53
Kartik Arora <kartikar@uw.edu>
2018-06-10 16:54:04 -0700

    create contributor table

--- createTables.sql
+++ createTables.sql
@@ -3,9 +3,32 @@ CREATE TABLE USER(
   password VARCHAR(50)
 );
 
-CREATE TABLE project(
+CREATE TABLE PROJECT(
   ID INT PRIMARY KEY,
   creator VARCHAR(30),
   name VARCHAR(30),
   createdOn VARCHAR(30) --Day/Month/Year/hour(24hr)/min/sec
 );
+
+CREATE TABLE CODE(
+  ID INT,
+  FileName varchar(30),
+  message varchar(50),
+  code String,
+  version int,
+  PRIMARY KEY(ID, FileName)
+);
+
+CREATE TABLE Version(
+  ID INT,
+  FileName varchar(30),
+  message varchar(50),
+  code String,
+  version int,
+  PRIMARY KEY(ID, FileName)
+);
+
+CREATE TABLE Contributers(
+  username VARCHAR(30),
+  pid INT FOREIGN KEY PROJECT(ID)
+)
